fa19821dc035973b00ee7650283d25b14aa735df
Hiraryo <nintendo640804@gmail.com>
2021-03-10 11:49:11 +0900

    Improved the range to generation enemies

--- Assets/Script/Enemy/Generater.cs
+++ Assets/Script/Enemy/Generater.cs
@@ -8,16 +8,17 @@ namespace Enemy
     {
         public GameObject EnemyPrefab;
         [SerializeField] private int NumberToGenerate;
-        [SerializeField] [Range(10f, 5f)] private float _positionX;
-        [SerializeField] [Range(10f, 5f)] private float _positionZ;
+        [SerializeField] [Range(5f, 10f)] private float _maxGenerationRadius;
+        [SerializeField] [Range(1f, 5f)] private float _minGenerationRadius;
 
         private void Update()
         {
             if (NumberToGenerate < 1)
                 return;
             Vector3 _generatePosition = transform.localPosition;
-            _generatePosition.x = Random.Range(_positionX, -_positionX);
-            _generatePosition.z = Random.Range(_positionZ, -_positionZ);
+            float _angle = Random.Range(0, 360);
+            _generatePosition.x = Random.Range(_minGenerationRadius * Mathf.Cos(_angle * Mathf.Deg2Rad),_maxGenerationRadius * Mathf.Cos(_angle * Mathf.Deg2Rad));
+            _generatePosition.z = Random.Range(_minGenerationRadius * Mathf.Sin(_angle * Mathf.Deg2Rad), _maxGenerationRadius * Mathf.Sin(_angle * Mathf.Deg2Rad));
             transform.localPosition = _generatePosition;
 
             Spawn();
