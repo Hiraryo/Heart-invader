88e631e46a76b33fd204501db007e4d14fcb6b1a
Daigo.Teramoto <52353757+TD826@users.noreply.github.com>
2021-03-17 20:00:52 +0900

    .metaファイルエラーとスクリプト複製のエラー解消

--- Assets/Scripts/Guns/Bullets/HandGunBullet.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HandGunBullet : Bullet
-{
-    public HandGunBullet()
-        : base(1f, 1f, (GameObject)Resources.Load("Bullets/3dObjects/HandGunBullet")) { }
-}
