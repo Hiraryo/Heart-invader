9c3c338f4b169250394082f2765060e8dc20c80d
Ryota Hirai <g1853806@cc.kyoto-su.ac.jp>
2021-03-28 04:34:19 +0900

    Modified the logic to generate enemies.

--- Assets/Script/Generate/Calculation.cs
+++ Assets/Script/Generate/Calculation.cs
@@ -8,20 +8,19 @@ namespace Generate
     public class Calculation : GenerateBase
     {
         public GameObject EnemyPrefab;
-        private float _generateTime,_elapsedTime; //生成時間、経過時間
-        [SerializeField] private int _numberToGenerate = 5; //敵の生成数
+        private float _generateInterval; //待ち時間
+        [SerializeField] private int _numberToGenerate = 10; //敵の生成数
         [SerializeField] [Range(5f, 10f)] private float _maxGenerationRadius;
         [SerializeField] [Range(1f, 5f)] private float _minGenerationRadius;
-        private void Start()
+        private IEnumerator Start()
         {
-            //生成時間(3〜5秒間)
-            _generateTime = Random.Range(3.0f, 5.0f);
-            //_numberToGenerate = EnemyManager.Instance.numberToGenerate;
-        }
-        private void Update()
-        {
-            _elapsedTime += Time.deltaTime;
-            if (_numberToGenerate > 0 && _elapsedTime > _generateTime){ CoordinateCalculation();}
+            while(_numberToGenerate > 0)
+            {
+                //待ち時間(3〜5秒間)
+                _generateInterval = Random.Range(3.0f, 5.0f);
+                CoordinateCalculation();
+                yield return new WaitForSeconds(_generateInterval);
+            }
         }
 
         private void CoordinateCalculation()
@@ -34,9 +33,6 @@ namespace Generate
             transform.localPosition = _generatePosition;
             Generate(EnemyPrefab, transform.TransformPoint(transform.localPosition), Quaternion.identity);
             _numberToGenerate--;
-            //生成時間(3〜5秒間)
-            _generateTime = Random.Range(3.0f, 5.0f);
-            _elapsedTime = 0;
         }
 
         public float DistanceCalculation(GameObject heroine, GameObject my)
